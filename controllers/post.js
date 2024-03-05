422c2c9feccab713d3b39e3880d2bd90de8c19e3
Joshtri <stuffofyos1516@gmail.com>
2024-03-05 16:26:23 +0800

    lts

--- controllers/post.js
+++ controllers/post.js
@@ -1,24 +1,24 @@
-import { PrismaClient } from "@prisma/client"
-const prisma = new PrismaClient();
+// import { PrismaClient } from "@prisma/client"
+// const prisma = new PrismaClient();
 
-export const createPost = async(req,res)=>{
+// export const createPost = async(req,res)=>{
 
-    const {
-        title,
-        content,
+//     const {
+//         title,
+//         content,
     
-    } = req.body;
+//     } = req.body;
 
-    try {
-        const newPost = await prisma.posts.create({
-            data:{
-                title,
-                content
-            }
-        });
-        res.status(201).json(newPost);
-    } catch (error) {
-        console.log(error);
-    }
-}
+//     try {
+//         const newPost = await prisma.posts.create({
+//             data:{
+//                 title,
+//                 content
+//             }
+//         });
+//         res.status(201).json(newPost);
+//     } catch (error) {
+//         console.log(error);
+//     }
+// }
 
